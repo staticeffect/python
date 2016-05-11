4a088abd5c50aacd9050526ceb9de355f5c8a278
staticeffect <brettgravois@gmail.com>
2016-05-11 07:13:28 -0700

    Python Starter Scripts

---
+++ ex3.py
@@ -0,0 +1,26 @@
+print "I will now cout my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it is False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+print 3.5 + 2.0 - 1.5
+print 3.456 - 1.54 + 2.0
